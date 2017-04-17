58b5fa69672a7cbc5f18aee5e9bfc1cee8de0814
Edgar Orendain <edgar@orendainx.com>
2017-04-17 13:28:43 -0700

    Updated deploy script

--- scripts/deploy-topology.sh
+++ scripts/deploy-topology.sh
@@ -2,4 +2,4 @@
 
 scriptDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd)"
 
-storm jar $scriptDir/trucking-storm-topology-java/target/trucking-storm-topology-java-0.3.2-shaded.jar com.orendainx.hortonworks.trucking.storm.java.topologies.KafkaToKafka
+storm jar $scriptDir/trucking-storm-topology-java/target/trucking-storm-topology-java-0.3.2.jar com.orendainx.hortonworks.trucking.storm.java.topologies.KafkaToKafka
